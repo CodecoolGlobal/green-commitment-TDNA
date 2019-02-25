246c962763410414494b6d700b2b007408580748
András Urbin <urbinandras@gmail.com>
2019-02-25 14:09:17 +0100

    basic server stuff

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,13 +1,33 @@
 package com.codecool;
 
+
+import com.codecool.client.Client;
+
+import java.util.stream.Collectors;
+
 public class Main {
+    private static final String CLIENT = "CLIENT";
+    private static final String SERVER = "SERVER";
 
-    public String getWelcomeString() {
-        return "Hi!";
-    }
 
-    public static void main(String[] args) {
-        Main main = new Main();
-        System.out.println(main.getWelcomeString());
+    public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            System.err.println("First argument must be there");
+        } else {
+            String mode = args[0];
+            try {
+                switch (mode) {
+                    case CLIENT:
+
+                    default:
+                        System.err.println("Mode not implemented: " + mode);
+                        break;
+                }
+            } catch (IllegalArgumentException ex) {
+                System.err.println("Wrong argument: " + ex.getMessage());
+            }
+        }
+
     }
 }
